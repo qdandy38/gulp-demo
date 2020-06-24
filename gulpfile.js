4908d1c4c622eeb89b5974812a118fded508c175
YunHsuan Tsai <qdandy38@gmail.com>
2020-06-24 14:27:37 +0800

    gulp4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var autoprefixer = require('autoprefixer');
-var mainBowerFiles = require('main-bower-files');
 var browserSync = require('browser-sync').create();
-var gulpSequence = require('gulp-sequence');
+// var gulpSequence = require('gulp-sequence');
 var minimist = require('minimist');
 var envOptions = {
     string: 'env',
@@ -13,15 +12,25 @@ var options = minimist(process.argv.slice(2), envOptions);
 
 // html
 gulp.task('moveHtml', function() {
-    gulp.src('./source/**/*.html')
+    return gulp.src('./source/**/*.html')
         .pipe($.plumber())
         .pipe(gulp.dest('./public/'))
         .pipe(browserSync.stream())
 });
 // 編譯pug
 gulp.task('pug', function() {
-    gulp.src('./source/**/*.pug')
+    return gulp.src('./source/**/*.pug')
         .pipe($.plumber())
+        .pipe($.data(function() {
+            var khData = require('./source/data/data.json');
+            var menu = require('./source/data/menu.json');
+            var source = {
+                "khData": khData,
+                "menu": menu
+            };
+
+            return source;
+        }))
         .pipe($.pug({
             // Your options in here.
             pretty: true
@@ -30,12 +39,15 @@ gulp.task('pug', function() {
         .pipe(browserSync.stream());
 });
 
+
 // 編譯scss
 gulp.task('sass', function() {
     return gulp.src('./source/scss/**/*.scss')
         .pipe($.plumber())
         .pipe($.sourcemaps.init())
-        .pipe($.sass().on('error', $.sass.logError))
+        .pipe($.sass({
+            includePaths: ['./node_modules/bootstrap/scss']
+        }).on('error', $.sass.logError))
         .pipe($.postcss([autoprefixer()]))
         .pipe($.if(options.env === 'production', $.cleanCss())) //如果輸出格式為prodyction，則壓縮
         .pipe($.sourcemaps.write('.'))
@@ -62,18 +74,16 @@ gulp.task('babel', () =>
 );
 
 // 將bower載入的套件輸出至自己的專案資料夾中
-gulp.task('bower', function() {
-    return gulp.src(mainBowerFiles())
-        .pipe(gulp.dest('./.tmp/vendors'))
-});
+// gulp.task('bower', function() {
+//     return gulp.src(mainBowerFiles())
+//         .pipe(gulp.dest('./.tmp/vendors'))
+// });
 
 // 將bower載入的套件合併為一隻js
-gulp.task('vendorJS', ['bower'], function() {
-    return gulp.src('./.tmp/vendors/**/**.js')
-        .pipe($.order([
-            'jquery.js',
-            'bootstrap.js'
-        ]))
+gulp.task('vendorJS', function() {
+    return gulp.src(['./node_modules/jquery/dist/jquery.min.js',
+            './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+        ])
         .pipe($.concat('vendors.js'))
         .pipe($.if(options.env === 'production', $.uglify({
             compress: {
@@ -84,33 +94,27 @@ gulp.task('vendorJS', ['bower'], function() {
 })
 
 // 建立伺服器
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./public",
-            reloadDebounce: 2000 // 重新整理間隔需2秒以上
-        }
-    });
-});
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: { baseDir: "./public" },
+//         reloadDebounce: 2000 // 重新整理間隔需2秒以上
+//     });
+// });
 
 // 壓縮圖片
 gulp.task('image-min', function() {
-    gulp.src('./source/img/*')
+    return gulp.src('./source/img/*')
         .pipe($.if(options.env === 'production', $.imagemin()))
         .pipe(gulp.dest('./public/img'))
 });
 
 // 監控，即時更新
-gulp.task('watch', function() {
-    gulp.watch('./source/scss/**/*.scss', ['sass']);
-    gulp.watch('./source/**/*.pug', ['pug']);
-    gulp.watch('./source/**/*.js', ['babel']);
-    // watch(['./source/scss/**/*.scss', './source/**/*.pug'], function() {
-    //     // 直接呼叫 sass 這個 Task
-    //     gulp.start('sass');
-    //     gulp.start('pug');
-    // });
-});
+// gulp.task('watch', function() {
+//     gulp.watch('./source/scss/**/*.html', ['moveHtml']);
+//     gulp.watch('./source/scss/**/*.scss', ['sass']);
+//     gulp.watch('./source/**/*.pug', ['pug']);
+//     gulp.watch('./source/**/*.js', ['babel']);
+// });
 
 // 部署到github上
 gulp.task('deploy', function() {
@@ -119,12 +123,38 @@ gulp.task('deploy', function() {
 });
 
 //刪除 .tmp public資料夾
-gulp.task('clean', function() {
-    return gulp.src(['./.tmp', './public'], { read: false })
+gulp.task('clean', () => {
+    return gulp.src(['./.tmp', './public'], { read: false, allowEmpty: true })
         .pipe($.clean());
 });
 
 // 完成專案後，最終執行 gulp build --env production
-gulp.task('build', gulpSequence('clean', 'moveHtml', 'pug', 'sass', 'babel', 'vendorJS', 'image-min'));
+// gulp.task('build', gulpSequence('clean', 'moveHtml', 'pug', 'sass', 'babel', 'vendorJS', 'image-min'));
 // 開發中執行gulp
-gulp.task('default', ['moveHtml', 'pug', 'sass', 'babel', 'vendorJS', 'image-min', 'browser-sync', 'watch']);
+// gulp.task('default', ['moveHtml', 'pug', 'sass', 'babel', 'vendorJS', 'image-min', 'browser-sync', 'watch']);
+// gulp 4.0
+gulp.task('build',
+    gulp.series(
+        'clean',
+        gulp.parallel('moveHtml', 'pug', 'sass', 'babel', 'vendorJS', 'image-min')
+    )
+)
+
+gulp.task('default',
+    gulp.series(
+        'clean',
+        gulp.parallel('pug', 'sass', 'babel', 'vendorJS'),
+        function(done) {
+            browserSync.init({
+                server: { baseDir: "./public" },
+                reloadDebounce: 2000 // 重新整理間隔需2秒以上
+            });
+
+            gulp.watch('./source/scss/**/*.html', gulp.series('moveHtml'));
+            gulp.watch('./source/scss/**/*.scss', gulp.series('sass'));
+            gulp.watch('./source/**/*.pug', gulp.series('pug'));
+            gulp.watch('./source/**/*.js', gulp.series('babel'));
+            done();
+        }
+    )
+)
